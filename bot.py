aa2d3c6394787e84aef41a2253a01690fe3a7b87
Vanderlei Dallagnolo <vanderlei.dallagnolo@ellevo.com>
2023-10-16 12:57:32 -0300

    ...

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import websocket
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m>"
 
-def on_open():
+def on_open(ws):
     print('opened connection')
 
-def on_close():
+def on_close(ws):
     print('close connection')
 
-def on_message():
+def on_message(ws):
     print('received message')
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+ws.run_forever(0)
