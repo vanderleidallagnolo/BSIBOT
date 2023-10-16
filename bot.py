e10dc5a76ce91397ed410cfffd623510e3fb81db
Vanderlei Dallagnolo <vanderlei.dallagnolo@ellevo.com>
2023-10-16 13:27:20 -0300

    ...

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 import websocket
 
-SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m>"
+SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
 def on_open(ws):
     print('opened connection')
 
 def on_close(ws):
-    print('close connection')
+    print('closed connection')
 
-def on_message(ws):
+def on_message(ws, message):
     print('received message')
 
-ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
-ws.run_forever(0)
+try:
+    # code that might raise an exception
+    ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+    ws.run_forever()
+except Exception as e:
+    print(e)
